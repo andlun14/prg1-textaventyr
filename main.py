ee96e0750cf52978d36a3d092df9e8f13253cdc4
Andreas Lundsten <andlun14@edu.umea.se>
2026-09-17 13:40:52 +0200

    bs

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-# Här skriver du ditt textäventyr
+import random
+import time
+import sys
+
+def slowPrint(string, speed=0.05):
+    for char in string:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(speed)
+
+def clear():
+    print("\033[H\033[J", end="")
+
+namn = input("vad är ditt namn?: ")
+
+slowPrint("intro intro intro...")
+
+clear()
+
+if input("Väljer du höger eller vänster?: ").lower() == "vänster":
+    print("aa okej")
+else:
+    print("nja brush irrono")
+
