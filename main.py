75076ccd863652aee7072613a94287a0b507fb28
Andreas Lundsten <andlun14@edu.umea.se>
2026-09-24 11:42:22 +0200

    massa nytt på main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def slowPrint(string, speed=0.075):
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(speed)
+    time.sleep(0.2)
 
 def clear():
     print("\033[H\033[J", end="")
@@ -54,6 +55,9 @@ def höger_tunnel():
     else:
         if höger_start == 2:
             slowPrint("Draken är fortfarande kvar, sovandes... ")
+            time.sleep(1)
+            slowPrint("Du går tillbaka till början...")
+            clear()
         else:
             slowPrint("Draken ligger på golvet, sovandes... ")
         slowPrint("")
@@ -63,11 +67,83 @@ def vänster_tunnel():
         slowPrint("Du har redan varit i vänster tunnel.")
     else:
         slowPrint("Du går ner den långa och mörka tunneln. Det ligger både människor och monster skelett utspritt längst med väggarna. ")
+        slowPrint("När du går längst med väggen så börjar du fundera på vad det var som dödade alla de här varelserna. ")
+        while True:
+            slowPrint("En av skeletten har en påse guld i sin hand. Kommer du ta det?(ja/nej): ")
+            val = input("")
+            if val.lower() == "ja":
+                slowPrint("Du har påsen med guld från skelettet... ")
+                guld = 1
+                break
+            elif val.lower() == "nej":
+                slowPrint("Du lämnar skelettet i fred. Kanske bättre att respektera de döda... ")
+                guld = 0
+                break
+            else:
+                print("fel inmatning.")
+        time.sleep(1)
+        clear()
+        slowPrint("Du börjar se ljuset i slutet av tunneln... ")
+        slowPrint("När du kommit fram så står du i ett rum som ser försvånadsvärt rent ut. ")
+        slowPrint("Det finns ett bord i mitten av rummet, vapen hänger längst med väggen och det finns en eldstad som brinner med en gryta som kokar... ")
+        slowPrint("Du hör ett fotsteg bakom dig, du vänder dig om raskt men du hinner inte se vem eller vad det är som slår dig i huvudet...")
+        time.sleep(2)
+        clear()
+        time.sleep(1)
+        slowPrint("När du vaknar till så sitter fastlindrad i rep på en stol. På andra sidan bordet ser du någon som sitter och äter. ")
+        slowPrint("Långsamt får du tillbaka din syn. Personen som sitter på andra sidan bordet visar sig att vara en annan människa. ")
+        slowPrint(f"Han frågar ditt namn... Du säger att du heter {namn}")
+        slowPrint("Han frågar om du har något vapen samt massor andra frågor, du svarar ärligt... ")
+        time.sleep(1)
+        clear()
+        slowPrint("Sen kommer den stora frågan, 'Vad gör du i den här grottan?'")
+        slowPrint("Du berättar allt. Han blir lite arg på dig eftersom du förstörde in- och utgången men han förstår vilken situation du är i... ")
+        if höger_start == 2:
+            slowPrint("Till slut så säger han att han kan hjälpa dig med att ta dig ut bakom draken. Men att det är väldigt riskabelt. ")
+        else:
+            slowPrint("Han berättar att det finns en annan väg ut. Men det är just där som draken är, oftast sovandes, men ibland kan den vakna och chansen att överleva en drakattack är otroligt smal. ")
+            time.sleep(1)
+            slowPrint("'Jag kan hjälpa dig', Säger han tillslut. 'Men det kommer kosta'")
+        slowPrint("Han befriar dig från repen. ")
+        time.sleep(1)
+        clear()
+        if guld == 1:
+            slowPrint("Du tar fram påsen med guld du plockat från skelettet och lägger det på bordet. ")
+            slowPrint("Du säger till honom att han förtjänar det om han hjälper dig ta dig ut... ")
+        else:
+            slowPrint("Du kommer på att du gick förbi ett skelett med en påse guld. Du ursäktar digsjälv från bordet sen går du och hämtar påsen... ")
+            slowPrint("Du lägger påsen på bordet och säger att han kan få den om du kommer ut.")
+        time.sleep(1)
+        clear()
+        slowPrint("Han tar snabbt upp påsen, kollar igenom den, sen nickar han åt dig." )
+        slowPrint("Han plockar ner några vapen från väggen och tar med sig det. 'Man vet aldrig vad som kan hända', säger han till sigsjälv... ")
+        time.sleep(1)
+        clear()
+
+def slut():
+    slowPrint("Ni går tillbaka till starten där du kom in. Han står och kollar på den blockerade in- och utgången en stund sen går han till höger.")
+    time.sleep(1)
+    clear()
+    if höger_start == 2:
+        slowPrint("Draken är fortfarande kvar, sovandes... ")
+    else:
+        slowPrint("Draken ligger på golvet där inne, sovandes... ")
+    slowPrint("Han säger till dig att chansen att draken vaknar är hög. Men att det är tyvärr eran enda väg ut nu... ")
+    slowPrint("Han börjar långsamt gå länst med väggen av rummet. Du följer efter.")
+    slowPrint("Ni rör er mot dörren bakom draken men när ni nästa är där så känner ni hur golvet skakar lite... ")
+    slowPrint("Ni vänder er om i sync och ser hur draken står upp, vaken, och flåsar åt er. ")
+    slowPrint("Även fast ni nästan är vid dörren så har ni inte tid att bryta upp plankorna som sitter fast över dörren. ")
+    slowPrint("Ni har bara ett val nu... ")
+    time.sleep(2)
+
+def drakstrids():
+    slowPrint()
 
 höger_start = 0
 vänster_start = 0
+guld = 0
 val = ""
-#namn = input("vad är ditt namn?: ")
+namn = input("vad är ditt namn?: ")
 
 #intro()
 #time.sleep(2)
@@ -96,11 +172,10 @@ while True:
                     val = vilken_tunnel()
                     if val.lower() == "höger":
                         höger_tunnel()
-                        time.sleep(1)
-                        slowPrint("Du går tillbaka till början...")
                     elif val.lower() == "vänster":
                         vänster_tunnel()
-
+                        slut()
+                        drakstrids()
                     else:
                         print("fel inmatning.")
             else:
@@ -108,5 +183,7 @@ while True:
     elif val.lower() == "vänster":
         vänster_start = 1
         vänster_tunnel()
+        slut()
+        drakstrids()
     else:
         print("fel inmatning.")
